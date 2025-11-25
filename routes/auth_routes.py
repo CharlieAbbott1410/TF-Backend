1b672e4a520cfdcc4ca91fdfa0b16d6ed295bb38
CharlieAbbott1410 <CharlieAbbott142007@gmail.com>
2025-11-25 15:52:46 +0000

    Database Setup

--- routes/auth_routes.py
+++ routes/auth_routes.py
@@ -1,14 +1,34 @@
 from fastapi import APIRouter, HTTPException
-from models.auth_models import LoginForm, RegisterForm
+from models.auth_models import LoginForm, RegisterForm, UserPublicData
+from db import db
 
 auth_router = APIRouter()
 
-@auth_router.post("/register")
-def register(register_data: RegisterForm):
-    new_user = register_data.model_dump()
-    return new_user
+users = db["users"] #reference to the collection
 
-@auth_router.post("/login")
+@auth_router.post("/register",response_model=UserPublicData)
+def register(register_data: RegisterForm):
+    if users.find_one({"email":register_data.email}):
+        raise HTTPException(status_code=400, detail="Email already exist")
+    
+    newUser = {
+        "email" : register_data.email,
+        "password" : register_data.password,
+        "username" : register_data.username,
+    }
+    result = users.insert_one(newUser)
+    print(result)
+    return {"email": register_data.email, "username":register_data.username}
+
+@auth_router.post("/login", response_model=UserPublicData)
 def login(login_data: LoginForm):
-    email = login_data.email
-    return {"temp": "temp"}
+    
+    foundUser = users.find_one({"email" : login_data.email})
+
+    if not foundUser or foundUser["password"] != login_data.password:
+        raise HTTPException(status_code=401, detail="Invalid Credentials Goober")
+    
+    return{
+        "username" : foundUser["username"],
+        "email" : foundUser["email"]
+    }
